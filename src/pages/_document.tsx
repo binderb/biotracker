dc6382d0e0ed90392674ae845861ef7271e578e6
Ben Binder <ben.binder11@gmail.com>
2023-04-19 11:18:47 -0500

    Add environment variable example.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href={process.env.BASE_PATH + '/favicon.ico'} />
+      </Head>
       <body className='font-primary'>
         <Main />
         <NextScript />
