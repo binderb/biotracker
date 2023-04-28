a3772af62ba3eb8582418cf758eec6dde625e013
Ben Binder <ben.binder11@gmail.com>
2023-04-28 10:14:44 -0500

    Add mock-up of 2-step study creator.

--- src/pages/study-creator.tsx
+++ src/pages/study-creator.tsx
@@ -33,6 +33,7 @@ const StudyCreator = () => {
   const [studyType, setStudyType] = useState('');
   const [nextStudy, setNextStudy] = useState(1);
   const [creatorStatus, setCreatorStatus] = useState('');
+  const [creatorStep, setCreatorStep] = useState(1);
   const { data: nextStudyData } = useQuery(GET_NEXT_STUDY, {
     variables: {
       clientCode: clientCode
@@ -89,10 +90,21 @@ const StudyCreator = () => {
           studyType: studyType
         }
       });
-      setCreatorStatus(`Completed adding new study: ${studyFullName}`)      
+      setCreatorStatus(`Completed adding new study: ${studyFullName}`);
+      setCreatorStep(3);      
     } catch (err:any) {
       setCreatorStatus(err.message);
     }
+  }
+
+  function handleChangeStep (delta:number) {
+    const newCreatorStep = creatorStep + delta;
+    if (newCreatorStep === 1 || newCreatorStep === 2) setCreatorStatus('');
+    if (newCreatorStep === 2 && (!clientCode || !nextStudy || !studyType)) {
+      setCreatorStatus('Please select a value for all fields before proceeding!');
+      return;
+    }
+    setCreatorStep(newCreatorStep);
   }
 
   return (
@@ -100,33 +112,67 @@ const StudyCreator = () => {
       <Navbar/>
       { status === 'authenticated' ?
       <main className="flex items-top p-4">
-        <div id="client-table" className='bg-secondaryHighlight p-4 rounded-xl flex-grow'>
+        <div id="create-study" className='bg-secondaryHighlight p-4 rounded-xl flex-grow'>
           <h1 className='mb-2'>Create New Study</h1>
-          <div className='flex items-center mb-2'>
-            <div className='mr-2'>Client Code:</div>
-            <select className='bg-[#ffffff88] p-2' onChange={handleClientChange}>
-              <option value=''>-- Choose --</option> 
-              {clientCodes.map( (client:Client) => 
-                <option value={client.code} key={client.code}>{client.code}</option>  
-              )}
-            </select>
-          </div>
-          <div className='flex items-center mb-2'>
-            <div className='mr-2'>Study Index (automatically generated):</div>
-            <div className='font-mono'>{nextStudy ? '00'+nextStudy : '(Choose a client)'}</div>
-          </div>
-          <div className='flex items-center mb-2'>
-            <div className='mr-2'>Study Type Code:</div>
-            <select className='bg-[#ffffff88] p-2' onChange={handleStudyTypeChange}>
-              <option value=''>-- Choose --</option>
-              <option value='HU'>HU - Human Cadaver</option>
-              <option value='IVT'>IVT - In Vitro</option>
-              <option value='INT'>INT - Animal Interventional</option>
-              <option value='SUR'>SUR - Animal Surgical</option>
-            </select>
-          </div>
-          <button className='std-button' onClick={handleSubmitNewStudy}>Submit</button>
-          <div className='my-2 text-[#800] whitespace-pre font-mono'>{creatorStatus}</div>
+          { (creatorStep === 1) &&
+            <section id='step-1'>
+            <h2 className='mb-2'>Basic Setup (Step 1 of 2)</h2>
+            <div className='flex items-center mb-2'>
+              <div className='mr-2'>Client Code:</div>
+              <select className='bg-[#ffffff88] p-2' onChange={handleClientChange} value={clientCode}>
+                <option value=''>-- Choose --</option> 
+                {clientCodes.map( (client:Client) => 
+                  <option value={client.code} key={client.code}>{client.code}</option>  
+                )}
+              </select>
+            </div>
+            <div className='flex items-center mb-2'>
+              <div className='mr-2'>Study Index (automatically generated):</div>
+              <div className='font-mono'>{nextStudy ? '00'+nextStudy : '(Choose a client)'}</div>
+            </div>
+            <div className='flex items-center mb-2'>
+              <div className='mr-2'>Study Type Code:</div>
+              <select className='bg-[#ffffff88] p-2' onChange={handleStudyTypeChange} value={studyType}>
+                <option value=''>-- Choose --</option>
+                <option value='HU'>HU - Human Cadaver</option>
+                <option value='IVT'>IVT - In Vitro</option>
+                <option value='INT'>INT - Animal Interventional</option>
+                <option value='SUR'>SUR - Animal Surgical</option>
+              </select>
+            </div>
+            <button className='std-button' onClick={() => handleChangeStep(1)}>Next</button>
+            <div className='my-2 text-[#800] whitespace-pre font-mono'>{creatorStatus}</div>          
+            </section>
+          }
+          { (creatorStep === 2) && 
+            <section id='step-2'>
+              <h2 className='mb-2'>Add Documents (Step 2 of 2)</h2>
+              <div className='flex items-center mb-2'>
+                <div className='mr-2'>Study Designation:</div>
+                <div className='font-mono'>{ ( clientCode && nextStudy && studyType ) ? `${clientCode}${nextStudy.toString().padStart(3,'0')}-${studyType}` : 'Missing Information!'}</div>
+              </div>
+              <div className='flex flex-col mb-2'>
+                <div className='mb-2'>Prepopulate Study Description:</div>
+                <textarea className='resize-none h-[100px] bg-[#FFFFFF88] p-2' placeholder='Add study description...'></textarea>
+              </div>
+              <div className='flex items-center mb-4'>
+                <div className='mr-2'>Add Forms:</div>
+                <select className='bg-[#ffffff88] p-2' onChange={handleStudyTypeChange}>
+                  <option value=''>{`Standard forms for ${studyType} study`}</option>
+                </select>
+              </div>
+              <button className='std-button mr-1' onClick={() => handleChangeStep(-1)}>Back</button>
+              {/* <button className='std-button' onClick={() => handleChangeStep(1)}>Next</button> */}
+              <button className='std-button' onClick={handleSubmitNewStudy}>Submit</button>
+              <div className='my-2 text-[#800] whitespace-pre font-mono'>{creatorStatus}</div>          
+            </section>
+          }
+          { (creatorStep === 3) && 
+            <section id='step-3'>
+              <div className='mb-4'>{creatorStatus}</div>
+              <button className='std-button mr-1' onClick={() => handleChangeStep(-2)}>Start Over</button>
+            </section>
+          }
         </div>
       </main>
       :
