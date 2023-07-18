bcbe9f7ab74718595f78c7178d33a28c81e6917a
Ben Binder <ben.binder11@gmail.com>
2023-07-18 15:27:54 -0500

    Accommodate base path for images in next.config.js.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.BASE_PATH,
+  images: {
+    path: `${process.env.BASE_PATH}/_next/image`,
+  }
 }
 
 module.exports = nextConfig
