56effcf76ad56be7ef12c63fdc5110bb8910eb88
Ben Binder <ben.binder11@gmail.com>
2023-07-19 14:20:34 -0500

    Test image resolution solution.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.BASE_PATH,
-  images: {
-    path: `${process.env.BASE_PATH}/_next/image`,
-  }
 }
 
 module.exports = nextConfig
